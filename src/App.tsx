34008460b31b631937538b496238a3c22c200382
treheinguy <treheinguy@gmail.com>
2023-04-25 15:38:55 -0500

    Added Solid Router and babel ts transformer to package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Routes, Route, A } from '@solidjs/router';
 import type { Component } from 'solid-js';
+import HeadToHeadPage from './Pages/HeadToHeadPage';
 
 const App: Component = () => {
   return (
     <>
-      <h1>Learn Solid</h1>
+      <A href={'/head-to-head'}>Head to Head</A>
+      <Routes>
+        <Route path='/' element={<div>Home</div>} />
+        <Route path='/head-to-head' element={<HeadToHeadPage />} />
+      </Routes>
     </>
   );
 };
